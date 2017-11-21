00e2d50076431ffa55e0833234371ebd3e257ba3
Tabebee <tabibone@gmail.com>
2017-11-21 15:30:59 -0500

    database logging burgers in console but not on page?

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,19 +1,33 @@
 $(function () {
-    function updateNewBurgers() {
-
-    };  //End updateNewBurgers
-
+    //  Put all tasks in functions
     function updateDevouredBurgers() {
 
     };  //End updateDevouredBurgers
 
-    function addNewBurger() {
-
+    function addNewBurger(burgerName) {
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: burgerName
+        }).then(
+            function () {
+                console.log("Added New Burger " + burgerName);
+                location.reload();
+            }
+        );
     };  //End addNewBurger
 
     function deleteBurger() {
 
     };  //End deleteBurger
+
+//     On click "Add Me"
+    $(".addMeButton").on("click", function () {
+        var burgerName = $("#newBurger").val().trim();
+        addNewBurger(burgerName);
+    });
+
+
+
 
 //    End jqeury function
 });
